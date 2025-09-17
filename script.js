f3a714df5911415b8ec27ab78aa9a960c6f33e62
Fiayz <sifathhossain115@gmail.com>
2025-09-17 23:49:32 +0600

    Feat: F to C convertion logic added

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ const kelvin = document.getElementById("kelvin");
 
 function computeTemp(e) {
   const currentEvent = e.target.value;
+
+  switch (currentEvent) {
+    case "celsius":
+      fahrenheit.value = currentEvent * 1.8 + 32;
+      kelvin.value = currentEvent * 273.15;
+      break;
+
+    case "fahrenheit":
+      celsius.value = (currentEvent - 32) * 1.8;
+  }
 }
+
+// K = °C + 273.15
+// °F = (°C × 1.8) + 32
+// C=(F−32)×1.8​
